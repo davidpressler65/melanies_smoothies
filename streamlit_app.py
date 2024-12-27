d94e2a590655fe2f09929401165315c47c6c4340
davidpressler65 <davidpressler@yahoo.com>
2024-12-27 14:09:02 -0500

    Update streamlit_app.py
    Moved data frame to the for each fruit loop.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -35,6 +36,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -45,11 +48,6 @@ if ingredients_list:
     #st.stop()
     time_to_insert = st.button('Submit Order')
 
-    import requests
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    # st.text(smoothiefroot_response.json())
-    sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
-    
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
